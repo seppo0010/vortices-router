6a2812832c8ec19758dd9170fbb15190c8b38836
Sebastian Waisbrot <seppo0010@gmail.com>
2019-10-08 23:20:43 -0300

    Test basic (not passing but complete)

--- go.mod
+++ go.mod
@@ -6,15 +6,16 @@ require (
 	github.com/google/gopacket v1.1.17
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/seppo0010/nfqueue-go v0.0.0-20190928072127-30b7098c8da7
-	github.com/seppo0010/vortices-dockercompose v0.0.0-20190928082356-ee40c2b9380a
+	github.com/seppo0010/vortices-dockercompose v0.0.0-20191009014255-7c909e89e007
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392 // indirect
 	golang.org/x/net v0.0.0-20190923162816-aa69164e4478 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20190927073244-c990c680b611 // indirect
+	golang.org/x/sys v0.0.0-20191008105621-543471e840be // indirect
 	golang.org/x/text v0.3.2 // indirect
 	golang.org/x/tools v0.0.0-20190925164712-ae58c0ff6b32 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- tests/basic/basic_test.go
+++ tests/basic/basic_test.go
@@ -1,12 +1,15 @@
 package basic
 
 import (
+	"fmt"
+	"io/ioutil"
 	"path"
 	"runtime"
 	"testing"
+	"time"
 
 	dc "github.com/seppo0010/vortices-dockercompose"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type Topology struct {
@@ -23,7 +26,7 @@ func basicInit(t *testing.T) *Topology {
 
 	compose := dc.NewCompose(dc.ComposeConfig{})
 	routerImage, err := compose.BuildDockerPath("router", path.Dir(path.Dir((path.Dir(filename)))))
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	topology := &Topology{Compose: compose}
 	topology.LAN = compose.AddNetwork("lan", dc.NetworkConfig{})
@@ -38,9 +41,9 @@ func basicInit(t *testing.T) *Topology {
 	})
 	computerImage, err := compose.BuildDocker("computer", `
 FROM ubuntu
-RUN apt update && apt install -y iproute2
+RUN apt update && apt install -y iproute2 netcat-openbsd
     `)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	computerConfig := dc.ServiceConfig{Image: computerImage, Command: []string{"sleep", "infinity"}}
 	topology.LANComputer = compose.AddService("lancomputer", computerConfig, []dc.ServiceNetworkConfig{
 		dc.ServiceNetworkConfig{Network: topology.LAN},
@@ -54,11 +57,43 @@ RUN apt update && apt install -y iproute2
 func TestBasic(t *testing.T) {
 	topology := basicInit(t)
 	err := topology.Compose.Start()
-	assert.Nil(t, err)
+	require.Nil(t, err)
+	defer topology.Compose.Stop()
+
 	err = topology.LANComputer.SudoExec("ip", "route", "del", "default").Run()
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	ipAddress, err := topology.Router.GetIPAddressForNetwork(topology.LAN)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	err = topology.LANComputer.SudoExec("ip", "route", "add", "default", "via", ipAddress).Run()
-	assert.Nil(t, err)
+	require.Nil(t, err)
+
+	server := topology.InternetComputer.Exec("bash", "-c", "echo hello |nc -u -l 8000 -W 1")
+	err = server.Start()
+	require.Nil(t, err)
+	defer func() {
+		server.Kill()
+	}()
+
+	require.Nil(t, err)
+	internetComputerIPAddress, err := topology.InternetComputer.GetIPAddressForNetwork(topology.Internet)
+	require.Nil(t, err)
+	cmd := topology.LANComputer.Exec("bash", "-c", fmt.Sprintf("echo ''| nc %s 8000 -u -W 1", internetComputerIPAddress))
+	stdoutPipe, err := cmd.StdoutPipe()
+	require.Nil(t, err)
+	err = cmd.Start()
+	require.Nil(t, err)
+	stdout := []byte{}
+	done := make(chan bool)
+	go func() {
+		stdout, err = ioutil.ReadAll(stdoutPipe)
+		done <- true
+	}()
+	select {
+	case <-time.After(3 * time.Second):
+		t.Fatal("timed out")
+	case <-done:
+	}
+
+	require.Nil(t, err)
+	require.Equal(t, string(stdout), "hello")
 }
