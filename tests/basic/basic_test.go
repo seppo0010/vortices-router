32572156158db576cfbf7a50f42d81fc1fa6efe2
Sebastian Waisbrot <seppo0010@gmail.com>
2019-10-20 13:38:04 -0300

    Testing busy port uses another one

--- tests/basic/basic_test.go
+++ tests/basic/basic_test.go
@@ -79,3 +79,50 @@ func TestBasic(t *testing.T) {
 
 	topology.PrintDebugIfFailed()
 }
+
+func TestBusyPortMaintainRange(t *testing.T) {
+	topology := tests.NewTopology(t)
+	err := topology.Compose.Start()
+	require.Nil(t, err)
+	defer topology.Compose.Clear()
+	defer topology.Compose.Stop()
+
+	routerWANIPAddress, err := topology.Router.GetIPAddressForNetwork(topology.Internet)
+	require.Nil(t, err)
+	lanComputerIPAddress, err := topology.LANComputer.GetIPAddressForNetwork(topology.LAN)
+	require.Nil(t, err)
+	internetComputerIPAddress, err := topology.InternetComputer.GetIPAddressForNetwork(topology.Internet)
+	require.Nil(t, err)
+	routerLANIPAddress, err := topology.Router.GetIPAddressForNetwork(topology.LAN)
+	require.Nil(t, err)
+
+	topology.LANComputer.SetDefaultGateway(routerLANIPAddress)
+
+	topology.StartTCPDump()
+
+	server := topology.Router.Exec("bash", "-c", `echo 'package main; import "io"; import "os"; import "net"; func main() { c, _ := net.ListenUDP("udp", &net.UDPAddr{Port:12345}); println("Listening on 12345"); io.Copy(os.Stdout, c)}' > server.go; go run server.go`)
+	serverOut, _ := server.StdoutPipe()
+	err = server.Start()
+	require.Nil(t, err)
+	defer func() {
+		server.Kill()
+	}()
+	serverOutBuffered := bufio.NewReader(serverOut)
+	line, _, err := serverOutBuffered.ReadLine()
+	expected := "Listening on"
+	if !strings.HasPrefix(string(line), expected) {
+		t.Fatalf("expected buffer (%s) to be (%s)", string(line), string(expected))
+	}
+
+	cmd := topology.LANComputer.Exec("bash", "-c", fmt.Sprintf("echo ''| nc %s 8000 -p 12345 -u -W 1 -w 0", internetComputerIPAddress))
+	err = cmd.Run()
+	require.Nil(t, err)
+
+	topology.ValidateSteps([]tests.Step{
+		tests.Step{Service: "lancomputer", SrcIP: net.ParseIP(lanComputerIPAddress), SrcPort: 12345, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+		tests.Step{Service: "router", SrcIP: net.ParseIP(lanComputerIPAddress), SrcPort: 12345, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+		tests.Step{Service: "router", SrcIP: net.ParseIP(routerWANIPAddress), SrcPort: 12347, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+	})
+
+	topology.PrintDebugIfFailed()
+}
