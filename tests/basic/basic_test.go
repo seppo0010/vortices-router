d3d3ca67ecb8a7ab19465854741ca6cf245a47b1
Sebastian Waisbrot <seppo0010@gmail.com>
2019-10-20 10:55:21 -0300

    Break down test into semi-reusable chunks

--- tests/basic/basic_test.go
+++ tests/basic/basic_test.go
@@ -3,153 +3,22 @@ package basic
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net"
-	"os"
-	"path"
-	"runtime"
 	"strings"
-	"sync"
-	"syscall"
 	"testing"
 	"time"
 
-	"github.com/google/btree"
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
-	dc "github.com/seppo0010/vortices-dockercompose"
-	"github.com/seppo0010/vortices-dockercompose/exec"
 	"github.com/seppo0010/vortices-router/tests"
 	"github.com/stretchr/testify/require"
 )
 
-type Topology struct {
-	LAN              *dc.Network
-	Internet         *dc.Network
-	Router           *dc.Service
-	LANComputer      *dc.Service
-	InternetComputer *dc.Service
-	Compose          *dc.Compose
-}
-
-func basicInit(t *testing.T) *Topology {
-	_, filename, _, _ := runtime.Caller(1)
-
-	compose := dc.NewCompose(dc.ComposeConfig{})
-	routerImage, err := compose.BuildDockerPath("router", path.Dir(path.Dir((path.Dir(filename)))))
-	require.Nil(t, err)
-
-	topology := &Topology{Compose: compose}
-	topology.LAN = compose.AddNetwork("lan", dc.NetworkConfig{})
-	topology.Internet = compose.AddNetwork("internet", dc.NetworkConfig{})
-	topology.Router = compose.AddService("router", dc.ServiceConfig{
-		Command:    []string{"./main", "--wan-alias", "wan", "--lan-alias", "lan"},
-		Image:      routerImage,
-		Privileged: true,
-	}, []dc.ServiceNetworkConfig{
-		dc.ServiceNetworkConfig{Network: topology.LAN, Aliases: []string{"lan"}},
-		dc.ServiceNetworkConfig{Network: topology.Internet, Aliases: []string{"wan"}},
-	})
-	computerImage, err := compose.BuildDocker("computer", `
-FROM ubuntu
-RUN apt update && apt install -y iproute2 netcat-openbsd iputils-ping tcpdump
-    `)
-	require.Nil(t, err)
-	computerConfig := dc.ServiceConfig{Image: computerImage, Command: []string{"sleep", "infinity"}}
-	topology.LANComputer = compose.AddService("lancomputer", computerConfig, []dc.ServiceNetworkConfig{
-		dc.ServiceNetworkConfig{Network: topology.LAN},
-	})
-	topology.InternetComputer = compose.AddService("internetcomputer", computerConfig, []dc.ServiceNetworkConfig{
-		dc.ServiceNetworkConfig{Network: topology.Internet},
-	})
-	return topology
-}
-
-type tcpdumpListener struct {
-	cmd               exec.Cmd
-	path              string
-	finishedWaitGroup *sync.WaitGroup
-	readyWaitGroup    *sync.WaitGroup
-}
-
-func tcpdump(t *testing.T, service *dc.Service, iface string) *tcpdumpListener {
-	l := &tcpdumpListener{}
-	l.cmd = service.Exec("tcpdump", "-i", iface, "-l", "--immediate-mode", "-w", "-", "udp")
-	out, _ := l.cmd.StdoutPipe()
-	_, _ = l.cmd.StderrPipe()
-	l.cmd.Start()
-	f, _ := ioutil.TempFile("", fmt.Sprintf("%s*.pcap", service.ContainerName))
-	l.path = f.Name()
-	l.finishedWaitGroup = &sync.WaitGroup{}
-	l.finishedWaitGroup.Add(1)
-	l.readyWaitGroup = &sync.WaitGroup{}
-	l.readyWaitGroup.Add(1)
-	go func() {
-		outBuffered := bufio.NewReader(out)
-		line, _, err := outBuffered.ReadLine()
-		if err != nil {
-			t.Fatal(err)
-		}
-		expected := "tcpdump: listening on"
-		if !strings.HasPrefix(string(line), expected) {
-			t.Fatalf("expected buffer (%s) to be (%s)", string(line), string(expected))
-		}
-		l.readyWaitGroup.Done()
-
-		io.Copy(f, outBuffered)
-		f.Close()
-		l.finishedWaitGroup.Done()
-	}()
-	return l
-}
-
 func TestBasic(t *testing.T) {
-	topology := basicInit(t)
+	topology := tests.NewTopology(t)
 	err := topology.Compose.Start()
 	require.Nil(t, err)
 	defer topology.Compose.Clear()
 	defer topology.Compose.Stop()
-
-	err = topology.LANComputer.SudoExec("ip", "route", "del", "default").Run()
-	require.Nil(t, err)
-	ipAddress, err := topology.Router.GetIPAddressForNetwork(topology.LAN)
-	require.Nil(t, err)
-	cmd := topology.LANComputer.SudoExec("ip", "route", "add", "default", "via", ipAddress)
-	stdoutPipe, err := cmd.StdoutPipe()
-	require.Nil(t, err)
-	err = cmd.Start()
-	require.Nil(t, err)
-	stdout, err := ioutil.ReadAll(stdoutPipe)
-	require.Equal(t, string(stdout), "")
-	err = cmd.Wait()
-	require.Nil(t, err)
-
-	type tcpdumpConfig struct {
-		service  *dc.Service
-		iface    string
-		listener *tcpdumpListener
-	}
-	tcpdumps := []*tcpdumpConfig{
-		&tcpdumpConfig{service: topology.LANComputer, iface: "eth0"},
-		&tcpdumpConfig{service: topology.Router, iface: "eth0"},
-		&tcpdumpConfig{service: topology.Router, iface: "eth1"},
-		&tcpdumpConfig{service: topology.InternetComputer, iface: "eth0"},
-	}
-	for _, td := range tcpdumps {
-		td.listener = tcpdump(t, td.service, td.iface)
-		defer os.Remove(td.listener.path)
-	}
-	for _, td := range tcpdumps {
-		td.listener.readyWaitGroup.Wait()
-	}
-
-	server := topology.InternetComputer.Exec("bash", "-c", "echo hello |nc -u -l 8000 -W 1")
-	err = server.Start()
-	require.Nil(t, err)
-	defer func() {
-		server.Kill()
-	}()
 
 	routerWANIPAddress, err := topology.Router.GetIPAddressForNetwork(topology.Internet)
 	require.Nil(t, err)
@@ -157,12 +26,33 @@ func TestBasic(t *testing.T) {
 	require.Nil(t, err)
 	internetComputerIPAddress, err := topology.InternetComputer.GetIPAddressForNetwork(topology.Internet)
 	require.Nil(t, err)
-	cmd = topology.LANComputer.Exec("bash", "-c", fmt.Sprintf("echo ''| nc %s 8000 -p 12345 -u -W 1", internetComputerIPAddress))
-	stdoutPipe, err = cmd.StdoutPipe()
+	routerLANIPAddress, err := topology.Router.GetIPAddressForNetwork(topology.LAN)
+	require.Nil(t, err)
+
+	topology.LANComputer.SetDefaultGateway(routerLANIPAddress)
+
+	topology.StartTCPDump()
+
+	server := topology.InternetComputer.Exec("bash", "-c", "echo hello |nc -u -l 8000 -W 1 -v")
+	serverOut, _ := server.StdoutPipe()
+	err = server.Start()
+	require.Nil(t, err)
+	defer func() {
+		server.Kill()
+	}()
+	serverOutBuffered := bufio.NewReader(serverOut)
+	line, _, err := serverOutBuffered.ReadLine()
+	expected := "Listening on"
+	if !strings.HasPrefix(string(line), expected) {
+		t.Fatalf("expected buffer (%s) to be (%s)", string(line), string(expected))
+	}
+
+	cmd := topology.LANComputer.Exec("bash", "-c", fmt.Sprintf("echo ''| nc %s 8000 -p 12345 -u -W 1", internetComputerIPAddress))
+	stdoutPipe, err := cmd.StdoutPipe()
 	require.Nil(t, err)
 	err = cmd.Start()
 	require.Nil(t, err)
-	stdout = []byte{}
+	stdout := []byte{}
 	done := make(chan bool)
 	go func() {
 		stdout, err = ioutil.ReadAll(stdoutPipe)
@@ -177,67 +67,15 @@ func TestBasic(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, string(stdout), "hello\n")
 
-	packets := btree.New(4)
-	for _, td := range tcpdumps {
-		td.listener.cmd.Signal(syscall.SIGINT)
-		td.listener.cmd.Wait()
-		td.listener.finishedWaitGroup.Wait()
-		handle, err := pcap.OpenOffline(td.listener.path)
-		if err != nil {
-			t.Errorf("error reading pcap in %s (%s): %s", td.service.ContainerName, td.iface, err.Error())
-			continue
-		}
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		for packet := range packetSource.Packets() {
-			packets.ReplaceOrInsert(&tests.PacketItem{
-				Packet:    packet,
-				Service:   td.service.ContainerName,
-				Interface: td.iface,
-			})
-		}
-	}
-
-	type step struct {
-		service string
-		srcIP   net.IP
-		srcPort int
-		dstIP   net.IP
-		dstPort int
-		payload []byte
-	}
-	currentStepIndex := 0
-	steps := []step{
-		step{service: "lancomputer", srcIP: net.ParseIP(lanComputerIPAddress), srcPort: 12345, dstIP: net.ParseIP(internetComputerIPAddress), dstPort: 8000, payload: []byte("\n")},
-		step{service: "router", srcIP: net.ParseIP(lanComputerIPAddress), srcPort: 12345, dstIP: net.ParseIP(internetComputerIPAddress), dstPort: 8000, payload: []byte("\n")},
-		step{service: "router", srcIP: net.ParseIP(routerWANIPAddress), srcPort: 12345, dstIP: net.ParseIP(internetComputerIPAddress), dstPort: 8000, payload: []byte("\n")},
-		step{service: "internetcomputer", srcIP: net.ParseIP(routerWANIPAddress), srcPort: 12345, dstIP: net.ParseIP(internetComputerIPAddress), dstPort: 8000, payload: []byte("\n")},
-		step{service: "internetcomputer", dstIP: net.ParseIP(routerWANIPAddress), dstPort: 12345, srcIP: net.ParseIP(internetComputerIPAddress), srcPort: 8000, payload: []byte("hello\n")},
-		step{service: "router", dstIP: net.ParseIP(routerWANIPAddress), dstPort: 12345, srcIP: net.ParseIP(internetComputerIPAddress), srcPort: 8000, payload: []byte("hello\n")},
-		step{service: "lancomputer", dstIP: net.ParseIP(lanComputerIPAddress), dstPort: 12345, srcIP: net.ParseIP(internetComputerIPAddress), srcPort: 8000, payload: []byte("hello\n")},
-	}
-	packets.Ascend(func(i btree.Item) bool {
-		if currentStepIndex >= len(steps) {
-			return false
-		}
-		pi := i.(*tests.PacketItem)
-		step := steps[currentStepIndex]
-		if pi.Service == step.service && pi.IPv4SrcIP().String() == step.srcIP.String() && pi.UDPSrcPort() == step.srcPort && pi.IPv4DstIP().String() == step.dstIP.String() && pi.UDPDstPort() == step.dstPort {
-			currentStepIndex += 1
-        }
-		return true
+	topology.ValidateSteps([]tests.Step{
+		tests.Step{Service: "lancomputer", SrcIP: net.ParseIP(lanComputerIPAddress), SrcPort: 12345, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+		tests.Step{Service: "router", SrcIP: net.ParseIP(lanComputerIPAddress), SrcPort: 12345, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+		tests.Step{Service: "router", SrcIP: net.ParseIP(routerWANIPAddress), SrcPort: 12345, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+		tests.Step{Service: "internetcomputer", SrcIP: net.ParseIP(routerWANIPAddress), SrcPort: 12345, DstIP: net.ParseIP(internetComputerIPAddress), DstPort: 8000, Payload: []byte("\n")},
+		tests.Step{Service: "internetcomputer", DstIP: net.ParseIP(routerWANIPAddress), DstPort: 12345, SrcIP: net.ParseIP(internetComputerIPAddress), SrcPort: 8000, Payload: []byte("hello\n")},
+		tests.Step{Service: "router", DstIP: net.ParseIP(routerWANIPAddress), DstPort: 12345, SrcIP: net.ParseIP(internetComputerIPAddress), SrcPort: 8000, Payload: []byte("hello\n")},
+		tests.Step{Service: "lancomputer", DstIP: net.ParseIP(lanComputerIPAddress), DstPort: 12345, SrcIP: net.ParseIP(internetComputerIPAddress), SrcPort: 8000, Payload: []byte("hello\n")},
 	})
 
-	if currentStepIndex < len(steps) {
-		t.Errorf("missing step %d: %#v", currentStepIndex, steps[currentStepIndex])
-	}
-	if t.Failed() {
-		logs, _ := topology.Compose.Logs()
-		print(logs)
-
-		packets.Ascend(func(i btree.Item) bool {
-			pi := i.(*tests.PacketItem)
-			fmt.Printf("s=%s i=%s p=(%s)\n", pi.Service, pi.Interface, pi.Packet.String())
-			return true
-		})
-	}
+	topology.PrintDebugIfFailed()
 }
---
+++ tests/topology.go
@@ -0,0 +1,216 @@
+package tests
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"sync"
+	"syscall"
+	"testing"
+
+	"github.com/google/btree"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	dc "github.com/seppo0010/vortices-dockercompose"
+	"github.com/seppo0010/vortices-dockercompose/exec"
+	"github.com/stretchr/testify/require"
+)
+
+type Service struct {
+	*dc.Service
+	T *testing.T
+}
+
+func (service *Service) SetDefaultGateway(ip string) {
+	err := service.SudoExec("ip", "route", "del", "default").Run()
+	require.Nil(service.T, err)
+	cmd := service.SudoExec("ip", "route", "add", "default", "via", ip)
+	stdoutPipe, err := cmd.StdoutPipe()
+	require.Nil(service.T, err)
+	err = cmd.Start()
+	require.Nil(service.T, err)
+	stdout, err := ioutil.ReadAll(stdoutPipe)
+	require.Equal(service.T, string(stdout), "")
+	err = cmd.Wait()
+	require.Nil(service.T, err)
+}
+
+type Topology struct {
+	LAN              *dc.Network
+	Internet         *dc.Network
+	Router           *Service
+	LANComputer      *Service
+	InternetComputer *Service
+	Compose          *dc.Compose
+	T                *testing.T
+	TCPDumps         []*tcpdumpConfig
+}
+
+func NewTopology(t *testing.T) *Topology {
+	_, filename, _, _ := runtime.Caller(1)
+
+	compose := dc.NewCompose(dc.ComposeConfig{})
+	routerImage, err := compose.BuildDockerPath("router", path.Dir(path.Dir((path.Dir(filename)))))
+	require.Nil(t, err)
+
+	topology := &Topology{Compose: compose, T: t}
+	topology.LAN = compose.AddNetwork("lan", dc.NetworkConfig{})
+	topology.Internet = compose.AddNetwork("internet", dc.NetworkConfig{})
+	topology.Router = &Service{T: t, Service: compose.AddService("router", dc.ServiceConfig{
+		Command:    []string{"./main", "--wan-alias", "wan", "--lan-alias", "lan"},
+		Image:      routerImage,
+		Privileged: true,
+	}, []dc.ServiceNetworkConfig{
+		dc.ServiceNetworkConfig{Network: topology.LAN, Aliases: []string{"lan"}},
+		dc.ServiceNetworkConfig{Network: topology.Internet, Aliases: []string{"wan"}},
+	})}
+	computerImage, err := compose.BuildDocker("computer", `
+FROM ubuntu
+RUN apt update && apt install -y iproute2 netcat-openbsd iputils-ping tcpdump
+    `)
+	require.Nil(t, err)
+	computerConfig := dc.ServiceConfig{Image: computerImage, Command: []string{"sleep", "infinity"}}
+	topology.LANComputer = &Service{T: t, Service: compose.AddService("lancomputer", computerConfig, []dc.ServiceNetworkConfig{
+		dc.ServiceNetworkConfig{Network: topology.LAN},
+	})}
+	topology.InternetComputer = &Service{T: t, Service: compose.AddService("internetcomputer", computerConfig, []dc.ServiceNetworkConfig{
+		dc.ServiceNetworkConfig{Network: topology.Internet},
+	})}
+	return topology
+}
+
+type tcpdumpListener struct {
+	Cmd               exec.Cmd
+	Path              string
+	FinishedWaitGroup *sync.WaitGroup
+	ReadyWaitGroup    *sync.WaitGroup
+}
+
+type tcpdumpConfig struct {
+	Service   *Service
+	Interface string
+	Listener  *tcpdumpListener
+}
+
+type Step struct {
+	Service string
+	SrcIP   net.IP
+	SrcPort int
+	DstIP   net.IP
+	DstPort int
+	Payload []byte
+}
+
+func tcpdump(t *testing.T, service *Service, iface string) *tcpdumpListener {
+	l := &tcpdumpListener{}
+	l.Cmd = service.Exec("tcpdump", "-i", iface, "-l", "--immediate-mode", "-w", "-", "udp")
+	out, _ := l.Cmd.StdoutPipe()
+	_, _ = l.Cmd.StderrPipe()
+	l.Cmd.Start()
+	f, _ := ioutil.TempFile("", fmt.Sprintf("%s*.pcap", service.ContainerName))
+	l.Path = f.Name()
+	l.FinishedWaitGroup = &sync.WaitGroup{}
+	l.FinishedWaitGroup.Add(1)
+	l.ReadyWaitGroup = &sync.WaitGroup{}
+	l.ReadyWaitGroup.Add(1)
+	go func() {
+		outBuffered := bufio.NewReader(out)
+		line, _, err := outBuffered.ReadLine()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := "tcpdump: listening on"
+		if !strings.HasPrefix(string(line), expected) {
+			t.Fatalf("expected buffer (%s) to be (%s)", string(line), string(expected))
+		}
+		l.ReadyWaitGroup.Done()
+
+		io.Copy(f, outBuffered)
+		f.Close()
+		l.FinishedWaitGroup.Done()
+	}()
+	return l
+}
+
+func (topology *Topology) StartTCPDump() {
+	topology.TCPDumps = []*tcpdumpConfig{
+		&tcpdumpConfig{Service: topology.LANComputer, Interface: "eth0"},
+		&tcpdumpConfig{Service: topology.Router, Interface: "eth0"},
+		&tcpdumpConfig{Service: topology.Router, Interface: "eth1"},
+		&tcpdumpConfig{Service: topology.InternetComputer, Interface: "eth0"},
+	}
+	for _, td := range topology.TCPDumps {
+		td.Listener = tcpdump(topology.T, td.Service, td.Interface)
+	}
+	for _, td := range topology.TCPDumps {
+		td.Listener.ReadyWaitGroup.Wait()
+	}
+}
+
+func (topology *Topology) Packets() []*PacketItem {
+	packets := btree.New(4)
+	for _, td := range topology.TCPDumps {
+		defer os.Remove(td.Listener.Path)
+		td.Listener.Cmd.Signal(syscall.SIGINT)
+		td.Listener.Cmd.Wait()
+		td.Listener.FinishedWaitGroup.Wait()
+		handle, err := pcap.OpenOffline(td.Listener.Path)
+		if err != nil {
+			topology.T.Errorf("error reading pcap in %s (%s): %s", td.Service.ContainerName, td.Interface, err.Error())
+			continue
+		}
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			packets.ReplaceOrInsert(&PacketItem{
+				Packet:    packet,
+				Service:   td.Service.ContainerName,
+				Interface: td.Interface,
+			})
+		}
+	}
+
+	packetList := make([]*PacketItem, 0, packets.Len())
+	packets.Ascend(func(i btree.Item) bool {
+		pi := i.(*PacketItem)
+		packetList = append(packetList, pi)
+		return true
+	})
+	return packetList
+}
+
+func (topology *Topology) ValidateSteps(steps []Step) {
+	currentStepIndex := 0
+	for _, pi := range topology.Packets() {
+		if currentStepIndex >= len(steps) {
+			break
+		}
+		step := steps[currentStepIndex]
+		if pi.Service == step.Service && pi.IPv4SrcIP().String() == step.SrcIP.String() && pi.UDPSrcPort() == step.SrcPort && pi.IPv4DstIP().String() == step.DstIP.String() && pi.UDPDstPort() == step.DstPort {
+			currentStepIndex += 1
+		}
+	}
+
+	if currentStepIndex < len(steps) {
+		topology.T.Errorf("missing step %d: %#v", currentStepIndex, steps[currentStepIndex])
+	}
+}
+
+func (topology *Topology) PrintDebugIfFailed() {
+	if topology.T.Failed() {
+		topology.PrintDebug()
+	}
+}
+func (topology *Topology) PrintDebug() {
+	logs, _ := topology.Compose.Logs()
+	print(logs)
+
+	for _, pi := range topology.Packets() {
+		fmt.Printf("s=%s i=%s p=(%s)\n", pi.Service, pi.Interface, pi.Packet.String())
+	}
+}
